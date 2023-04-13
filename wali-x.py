dc80dcddfe2ea83888830111fffd01028566c715
kunalmannu <112188096+kunalmannu@users.noreply.github.com>
2023-04-13 12:10:38 -0400

    Add files via upload

---
+++ wali-x.py
@@ -0,0 +1,23 @@
+import os
+
+print("""\033[96m
+
+░██╗░░░░░░░██╗░█████╗░██╗░░░░░██╗░░░░░░██╗░░██╗
+░██║░░██╗░░██║██╔══██╗██║░░░░░██║░░░░░░╚██╗██╔╝
+░╚██╗████╗██╔╝███████║██║░░░░░██║█████╗░╚███╔╝░
+░░████╔═████║░██╔══██║██║░░░░░██║╚════╝░██╔██╗░
+░░╚██╔╝░╚██╔╝░██║░░██║███████╗██║░░░░░░██╔╝╚██╗
+░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚══════╝╚═╝░░░░░░╚═╝░░╚═╝
+                  v1.0
+           |---[  By: KunalMannu ]---|       
+    """)
+    
+print("\033[93m IMPORTANT! RENAME YOUR DESIRE IMAGE TO [background] SMALLCAPS WITHOUT BRACKETS AND THE FILE TYPE LIKE png AND jpeg")    
+
+ip = input("\033[96m Enter Path To Image: ")
+
+os.system('sudo rm /usr/share/desktop-base/kali-theme/login/background')
+
+os.system('sudo cp '+ ip +' /usr/share/desktop-base/kali-theme/login/')
+
+print("DONE!")
